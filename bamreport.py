e1772fe0ab04d842b1b95cc5475381f02eeda8b1
MikeCox13 <mcox9813@yahoo.com>
2021-02-05 17:19:19 -0500

    added input text field: not working correctly

--- bamreport.py
+++ bamreport.py
@@ -27,7 +27,7 @@ cal.grid(column = 0 , row = 2)
 def grab_date():
     cal_label.config(text = cal.get_date())
 
-cal_button = Button(root, text = "Get Date", command = grab_date)
+cal_button = Button(root, text = "Select Date", command = grab_date)
 cal_button.grid(column = 0, row = 3)
 
 cal_label = Label(root, text = "")
@@ -36,7 +36,7 @@ cal_label.grid(column = 0, row = 4)
 
 
 #creating a dropdown menu
-def show():
+def show2():
     lbl2 = Label(root, text=clicked1.get()).grid(column = 1, row = 3)
 
 clicked1 = StringVar()
@@ -52,13 +52,13 @@ options1 = [
 drop = OptionMenu(root, clicked1, *options1)
 drop.grid(column = 1, row = 1)
 
-#need to fix issue where if user clicks button, the option selected will print over the last
-myButton = Button(root, text = "show selection", command = show).grid(column = 1, row = 2)
+#displays selection
+myButton = Button(root, text = "show selection", command = show2).grid(column = 1, row = 2)
 
 
 
 #creating a dropdown menu
-def show():
+def show3():
     lbl3 = Label(root, text=clicked2.get()).grid(column = 2, row = 3)
 
 clicked2 = StringVar()
@@ -74,13 +74,13 @@ options2 = [
 drop = OptionMenu(root, clicked2, *options2)
 drop.grid(column = 2, row = 1)
 
-#need to fix issue where is user cclicks button, the option will print over the last
-myButton = Button(root, text = "show selection", command = show).grid(column = 2, row = 2)
+#displays selection
+myButton = Button(root, text = "show selection", command = show3).grid(column = 2, row = 2)
 
 
 
 #creating a dropdown menu
-def show():
+def show4():
     lbl4 = Label(root, text=clicked3.get()).grid(column = 3, row = 3)
 
 clicked3 = StringVar()
@@ -96,8 +96,22 @@ options3 = [
 drop = OptionMenu(root, clicked3, *options3)
 drop.grid(column = 3, row = 1)
 
-#need to fix issue where if user clicks button, the option selected will print over the las
-myButton = Button(root, text = "show selection", command = show).grid(column = 3, row = 2)
+#displays selection
+myButton = Button(root, text = "show selection", command = show4).grid(column = 3, row = 2)
+
+
+
+#making an input text field
+def retrieve_input():
+    inputvalue = textbox.get("1.0",'end-1c')
+
+
+textbox = Text(root, height = 1, width = 10)
+textbox.grid(column = 4, row = 1)
+    
+button_TB = Button(root, height = 1, width = 10, text = "Confirm Text", command = lambda: retrieve_input())
+button_TB.grid(column = 4, row = 2)
+
 
 #call the mainloop funtion to run the program
 root.mainloop()
